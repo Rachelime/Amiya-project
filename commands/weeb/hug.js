5de31c173b97274df201d05c46ba973931849a4f
AUSGEFUHRT <74294520+AUSGEFUHRT@users.noreply.github.com>
2021-02-04 15:34:58 +0530

    Create hug.js

---
+++ commands/weeb/hug.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const Neko = require('neko-love')
+const axios = require('axios')
+module.exports = {
+  name: 'hug',
+  descreption: 'Hug tightly to someone!',
+  cooldown: 3,
+  async execute(message, args){
+    if(message.mentions.users.size){
+	  	taggedUser = message.mentions.users.first()
+             const response = await axios.get('https://neko-love.xyz/api/v1/hug');    
+       const embed = new Discord.MessageEmbed()
+      .setDescription(`Here is a nice hug for you ${taggedUser.username}!`)
+            .setColor(`RANDOM`)
+            .setImage(response.data.url)
+            .setTimestamp()
+            .setFooter('Powered by Neko-Love')
+             message.channel.send(embed)
+    }
+   else{
+      message.channel.send("Please tag a valid user!")
+   }
+  }
+}
