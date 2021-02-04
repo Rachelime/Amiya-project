d067e9f176f0bc5d5c3f85273e2db8a5dc5b709c
AUSGEFUHRT <74294520+AUSGEFUHRT@users.noreply.github.com>
2021-02-04 18:28:01 +0530

    Create baka.js

---
+++ commands/weeb/baka.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+const nya = require('nekos.life')
+const neki = new nya()
+module.exports = {
+  name: 'baka',
+  descreption: 'BAKA!',
+  cooldown: 3,
+  async execute(message, args){
+    
+    	  if(message.mentions.users.size){
+	  	taggedUser = message.mentions.users.first()
+
+    let baka = await neki.sfw.baka()
+       const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(`Hey ${taggedUser.username} BAKA!`)
+            .setImage(baka.url)
+            .setTimestamp()
+            .setFooter('Nekos.life | ' +  `${Date.now() - message.createdTimestamp}` + 'ms' , 'https://avatars2.githubusercontent.com/u/34457007?s=200&v=4');
+
+            message.channel.send(embed);
+        }
+        else{
+              let baka = await neki.sfw.baka()
+       const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(`Hey ${message.author.username} BAKA!`)
+            .setImage(baka.url)
+            .setTimestamp()
+            .setFooter('Nekos.life | ' +  `${Date.now() - message.createdTimestamp}` + 'ms' , 'https://avatars2.githubusercontent.com/u/34457007?s=200&v=4');
+
+            message.channel.send(embed);
+        }
+  
+  }
+}
