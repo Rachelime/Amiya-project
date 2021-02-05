e9c061435d70de349229b45cce87804c7bdc409c
AUSGEFUHRT <74294520+AUSGEFUHRT@users.noreply.github.com>
2021-02-05 23:35:17 +0530

    Update tickle.js

--- commands/weeb/tickle.js
+++ commands/weeb/tickle.js
@@ -6,9 +6,10 @@ module.exports = {
   descreption: 'Tickle! hehe!',
   cooldown: 3,
   async execute(message, args){
-    
-    	  if(message.mentions.users.size){
+   taggedUser = message.author
+ 	  if(message.mentions.users.size > 0){
 	  	taggedUser = message.mentions.users.first()
+     }
 
     let tickle = await neki.sfw.tickle()
        const embed = new Discord.MessageEmbed()
@@ -19,18 +20,8 @@ module.exports = {
             .setFooter('Nekos.life | ' +  `${Date.now() - message.createdTimestamp}` + 'ms' , 'https://avatars2.githubusercontent.com/u/34457007?s=200&v=4');
 
             message.channel.send(embed);
-        }
-        else{
-              let tickle = await neki.sfw.tickle()
-       const embed = new Discord.MessageEmbed()
-            .setColor('RANDOM')
-            .setDescription(`Tickle to ${message.author.username}!`)
-            .setImage(tickle.url)
-            .setTimestamp()
-            .setFooter('Nekos.life | ' +  `${Date.now() - message.createdTimestamp}` + 'ms' , 'https://avatars2.githubusercontent.com/u/34457007?s=200&v=4');
+        
 
-            message.channel.send(embed);
-        }
-  
   }
+}
 }
